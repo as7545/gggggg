55f35f5ca7c10272b8791e1bcf5f7b413d318394
as7545 <90466921+as7545@users.noreply.github.com>
2021-09-17 14:02:33 +0530

    Add files via upload

---
+++ class max.java
@@ -0,0 +1,17 @@
+class max
+{
+    public static void main(string[]){
+        int a=54,b-86;
+        if(a>b)
+        {
+            system.out.print(a);
+
+        }
+        else
+        {
+            system.out.print(b);
+
+        }
+    }
+
+}
