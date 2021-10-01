285764c9b5f678c7d9934152a94d6ca5d04a2d89
as7545 <90466921+as7545@users.noreply.github.com>
2021-10-01 15:42:42 +0530

    Add files via upload

---
+++ rev.java
@@ -0,0 +1,14 @@
+package hhhh;
+
+public class rev {
+
+	public static void main(String[] args) {
+String str = "Geekster";
+		
+		StringBuilder sBuilder = new StringBuilder(str);
+		
+		System.out.println(sBuilder.reverse());
+
+	}
+
+}
